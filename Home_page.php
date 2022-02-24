6094184f711d849aa82c893420f19a48830227de
bmlcrsgunasekara <97513246+bmlcrsgunasekara@users.noreply.github.com>
2022-02-23 20:11:47 -0800

    This is 23551's Home page code

---
+++ Home_page.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+</head>
+
+<body style="font-family: 'Times New Roman';">
+    <div class="container-fluid">
+        <div class="row ">
+            <div class="col-12 text-center bg-dark shadow p-3 mb-5  rounded">
+             
+
+                <h3 class="text-white">No More Hungry Tummies</h3>
+                <hr style="color: rgb(55, 225, 255);">
+
+            </div>
+
+        </div>
+        <div class="row mt-4">
+            <div class="col-8">
+                <h3>No More Hungry Tummies</h3>
+            <p style="font-family: calibri;">Most people would agree that tackling food poverty isn't just about access to
+                healthy affordable food.
+                <br>
+                <br>
+                Whilst this is a big part of the solution, only doing this will not fix the
+                problem of malnutrition, and many neglect the impact of lack of equipment,
+                confidence, and enough money to pay for firing up the oven. In Plymouth, they're
+                offering a more holistic approach.
+                 <br> <br>
+                 We can help change this by encouraging people to grow and cook their own fresh fruit and vegetables, and making it easier and cheaper for people to do this
+            </p>
+            </div>
+            
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-5 offset-1 offset-lg-0 col-lg-3">
+                <div class="card text-center" style="width: 18rem; border: none; outline: none; ">
+                    <img src="WhatsApp Image 2022-02-23 at 9.29.48 PM.jpeg" class="card-img-top" style=" height:200px;" alt="...">
+                    <div class="card-body">
+
+
+                        <a href="rural.php" class="btn btn-primary">Rural Development</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 offset-1 offset-lg-0 col-lg-3">
+                <div class="card text-center" style="width: 18rem; border: none; outline: none;">
+                    <img src="s.a.zero.jpeg" class="card-img-top" style=" height:200px;" alt="...">
+                    <div class="card-body">
+
+
+                        <a href="S.a.zero.hunger.php" class="btn btn-primary">Sustainable agriculture for zero hunger</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 offset-1 offset-lg-0 col-lg-3">
+                <div class="card text-center" style="width: 18rem; border: none; outline: none;  ">
+                    <img src="s.agri.jpeg" class="card-img-top" style=" height:200px;" alt="...">
+                    <div class="card-body">
+
+
+                        <a href="s.agriculturing.php" class="btn btn-primary">sustainable agriculturing</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 offset-1 offset-lg-0 col-lg-3">
+                <div class="card text-center" style="width: 18rem; border: none; outline: none; ">
+                    <img src="download (6).jpg" class="card-img-top " style=" height:200px;" alt="...">
+                    <div class="card-body">
+
+
+                        <a href="FoodandNutrition.php" class="btn btn-primary">Food security and Nutrition</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
